096701ade7b7ffb04fabf7635da4e401d13b8fdf
Luis Maduro <luisr@iconos.com.ve>
2025-10-13 10:39:34 -0400

    Initial commit

---
+++ src/mathpath/Mathpath.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mathpath;
+
+/**
+ *
+ * @author luisr
+ */
+public class Mathpath {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
